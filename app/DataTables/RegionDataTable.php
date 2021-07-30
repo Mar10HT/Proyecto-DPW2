2f5ad66ac50baef8e01bcf3b1a9d3cf9730d2776
Anthony Portillo <80374894+T0n7sh7@users.noreply.github.com>
2021-07-29 19:27:58 -0600

    Add files via upload

---
+++ app/DataTables/RegionDataTable.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\DataTables;
+
+use App\Models\Region;
+use Yajra\DataTables\Html\Button;
+use Yajra\DataTables\Html\Column;
+use Yajra\DataTables\Html\Editor\Editor;
+use Yajra\DataTables\Html\Editor\Fields;
+use Yajra\DataTables\Services\DataTable;
+
+class RegionDataTable extends DataTable
+{
+    /**
+     * Build DataTable class.
+     *
+     * @param mixed $query Results from query() method.
+     * @return \Yajra\DataTables\DataTableAbstract
+     */
+    public function dataTable($query)
+    {
+        return datatables()
+            ->eloquent($query)
+            ->addColumn('action', 'region.action');
+    }
+
+    /**
+     * Get query source of dataTable.
+     *
+     * @param \App\Models\Region $model
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function query(Region $model)
+    {
+        return $model->newQuery();
+    }
+
+    /**
+     * Optional method if you want to use html builder.
+     *
+     * @return \Yajra\DataTables\Html\Builder
+     */
+    public function html()
+    {
+        return $this->builder()
+                    ->setTableId('region-table')
+                    ->columns($this->getColumns())
+                    ->minifiedAjax()
+                    ->orderBy(1);
+    }
+
+    /**
+     * Get columns.
+     *
+     * @return array
+     */
+    protected function getColumns()
+    {
+        return [
+            'id',
+            'nombre',
+            'cant_depto'
+        ];
+    }
+
+    /**
+     * Get filename for export.
+     *
+     * @return string
+     */
+    protected function filename()
+    {
+        return 'Region_' . date('YmdHis');
+    }
+}
